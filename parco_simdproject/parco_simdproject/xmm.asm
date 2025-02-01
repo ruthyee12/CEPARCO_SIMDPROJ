b33ff257f6b52b7bc5e7b416932b7f485e2923b3
huhduhbuh <theoni_lim@dlsu.edu.ph>
2025-02-02 00:09:20 +0800

    xmm with weird n (lv did this entire file btw)

--- parco_simdproject/parco_simdproject/xmm.asm
+++ parco_simdproject/parco_simdproject/xmm.asm
@@ -10,9 +10,17 @@ xmm:
     ; r8 - B (pointer to vector B)
     ; r9 - result (pointer to store result)
 
+    ;register initializations
+    mov r11, rcx ; r11 has the true n value count
     xor r10, r10
-    shr rcx, 1
     xorpd xmm2, xmm2
+    shr rcx, 1
+
+    cmp r11, 0 ; to check for negative or non values
+    jle done
+
+    cmp r11, 1 ; check if only one
+    je BUT_THE_ONE_IN_FRONT_OF_THE_GUN_LIVES_FOREVER
 
 ADD_LOOP_XMM:
     vmovupd xmm0, [rdx+r10*8]
@@ -21,7 +29,16 @@ ADD_LOOP_XMM:
     vhaddpd xmm0, xmm0
     addsd xmm2, xmm0
     add r10, 2
+    sub r11, 2
     LOOP ADD_LOOP_XMM
+
+BUT_THE_ONE_IN_FRONT_OF_THE_GUN_LIVES_FOREVER:
+    cmp r11, 0
+    jle done    ; if there is remaining, add the last
+    movsd xmm0, [rdx+r10*8]
+    movsd xmm1, [r8+r10*8]
+    mulsd xmm0, xmm1
+    addsd xmm2, xmm0
 
 done:
     movq [r9], xmm2        
